7c1e370133f47e9388c92becc243d1a3313791ba
Sean Thomas Burke <hawaiianchimp@gmail.com>
2014-08-19 19:39:30 -0700

    Attempt to get this.xml working

--- index.js
+++ index.js
@@ -2,3 +2,10 @@ var Entry = require("./lib/entry");
 
 var CBS = new Entry("http://www.cbs.com/sitemaps/show/show_siteMap_index.xml");
 var walmart = new Entry("http://www.cbs.com/sitemaps/show/show_siteMap_index.xml");
+
+console.log(CBS);
+console.log(walmart);
+
+
+console.log(CBS.getSites());
+console.log(walmart.getSites());
