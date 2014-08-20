820b5bbb5275c194600d62586142fd332746239d
Sean Burke <seantomburke@users.noreply.github.com>
2014-08-19 19:01:48 -0700

    First working prototype

--- index.js
+++ index.js
@@ -1,7 +1,4 @@
 var Entry = require("./lib/entry");
 
 var CBS = new Entry("http://www.cbs.com/sitemaps/show/show_siteMap_index.xml");
-var walmart = new Entry("http://www.walmart.com/sitemap_cp.xml");
-
-console.log(CBS);
-console.log(walmart);
+var walmart = new Entry("http://www.cbs.com/sitemaps/show/show_siteMap_index.xml");
