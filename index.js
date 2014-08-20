d27dc8079abc04da4f681215a54a68eaf13e619e
Sean Burke <seantomburke@users.noreply.github.com>
2014-08-19 18:58:08 -0700

    Implementing more Object Orientation

--- index.js
+++ index.js
@@ -1,4 +1,7 @@
-var entry = require("./lib/entry");
+var Entry = require("./lib/entry");
 
-entry("http://www.cbs.com/sitemaps/show/show_siteMap_index.xml");
-entry("http://www.cbs.com/sitemaps/show/show_sitemap.xml");
+var CBS = new Entry("http://www.cbs.com/sitemaps/show/show_siteMap_index.xml");
+var walmart = new Entry("http://www.walmart.com/sitemap_cp.xml");
+
+console.log(CBS);
+console.log(walmart);
