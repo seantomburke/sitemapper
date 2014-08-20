075998a615f99e31aa983c816a05fc0e809e1097
Sean Burke <seantomburke@users.noreply.github.com>
2014-08-20 16:00:54 -0700

    Restructured, working site maps files

--- index.js
+++ index.js
@@ -1,11 +1,17 @@
-var Entry = require("./lib/entry");
+var sitemap = require("./lib/sitemap");
 
-var CBS = new Entry("http://www.cbs.com/sitemaps/show/show_siteMap_index.xml");
-var walmart = new Entry("http://www.cbs.com/sitemaps/show/show_siteMap_index.xml");
+sitemap.getSites("http://www.cbs.com/sitemaps/show/show_siteMap_index.xml", function(sites){
+	console.log(sites);
+});
 
-console.log(CBS);
-console.log(walmart);
+sitemap.getSites("http://www.cnn.com/sitemaps/sitemap-index.xml", function(err,sites){
+ 	console.log(sites);
+ });
 
+sitemap.getSites("http://www.cbs.com/sitemaps/show/show_siteMap_index.xml", function(err,sites){
+ 	console.log(sites);
+ });
 
-console.log(CBS.getSites());
-console.log(walmart.getSites());
+sitemap.getSites("http://www.walmart.com/sitemap_tp.xml", function(err,sites){
+ 	console.log(sites);
+ });
