cd2977b930605f023530e0336fc18813a3cfb0da
Sean Thomas Burke <hawaiianchimp@gmail.com>
2014-08-21 13:45:03 -0700

    trying rakuten

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 var sitemap = require("./lib/sitemap");
 
-sitemap.getSites("http://www.cbs.com/sitemaps/show/show_siteMap_index.xml", function(err, sites){
-	if(!err)console.log(sites);else console.log(err);
-});
+// sitemap.getSites("http://www.cbs.com/sitemaps/show/show_siteMap_index.xml", function(err, sites){
+// 	if(!err)console.log(sites);else console.log(err);
+// });
 
 // sitemap.getSites("http://www.cnn.com/sitemaps/sitemap-index.xml", function(err,sites){
 //  	if(!err)console.log(sites);else console.log(err);
 // });
 
-sitemap.getSites("http://www.walmart.com/sitemap_tp1.xml.gz", function(err,sites){
+sitemap.getSites("http://www.rakuten.com/sitemapxml/sitemapindex.xml", function(err,sites){
  	if(!err)console.log(sites);else console.log(err);
 });
