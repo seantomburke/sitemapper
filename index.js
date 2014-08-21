cafaab7df7cc36103195e65b4e9c0dcb1aa4092f
Sean Thomas Burke <hawaiianchimp@gmail.com>
2014-08-20 18:49:15 -0700

    Fixing example

--- index.js
+++ index.js
@@ -1,33 +1,13 @@
 var sitemap = require("./lib/sitemap");
 
-// sitemap.getSites("http://www.cbs.com/sitemaps/show/show_siteMap_index.xml", function(err, sites){
-// 	if(!err){
-// 		console.log(sites);
-// 	}
-// 	else 
-// 		console.log(err);
-// });
-
-sitemap.getSites("http://www.cnn.com/sitemaps/sitemap-index.xml", function(err,sites){
- 	if(!err){
-		console.log(sites);
-	}
-	else 
-		console.log(err);
+sitemap.getSites("http://www.cbs.com/sitemaps/show/show_siteMap_index.xml", function(err, sites){
+	if(!err)console.log(sites);else console.log(err);
 });
 
-// sitemap.getSites("http://www.cbs.com/sitemaps/show/show_siteMap_index.xml", function(err,sites){
-//  	if(!err){
-// 		console.log(sites);
-// 	}
-// 	else 
-// 		console.log(err);
+// sitemap.getSites("http://www.cnn.com/sitemaps/sitemap-index.xml", function(err,sites){
+//  	if(!err)console.log(sites);else console.log(err);
 // });
 
-// sitemap.getSites("http://www.walmart.com/sitemap_tp.xml", function(err,sites){
-//  	if(!err){
-// 		console.log(sites);
-// 	}
-// 	else 
-// 		console.log(err);
-// });
+sitemap.getSites("http://www.walmart.com/sitemap_tp1.xml.gz", function(err,sites){
+ 	if(!err)console.log(sites);else console.log(err);
+});
