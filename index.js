0745cf848322968e19ff25540df2e0d827d23f40
Sean Burke <seantomburke@users.noreply.github.com>
2015-05-09 16:21:20 +0000

    Changing the index.js file to run CBS instead of walmart

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 var sitemap = require("./lib/sitemap");
 
-// sitemap.getSites("http://www.cbs.com/sitemaps/show/show_siteMap_index.xml", function(err, sites){
-// 	if(!err)console.log(sites);else console.log(err);
-// });
+sitemap.getSites("http://www.cbs.com/sitemaps/show/show_siteMap_index.xml", function(err, sites){
+	if(!err)console.log(sites);else console.log(err);
+});
 
 // sitemap.getSites("http://www.cnn.com/sitemaps/sitemap-index.xml", function(err,sites){
 //  	if(!err)console.log(sites);else console.log(err);
 // });
 
-sitemap.getSites("http://www.walmart.com/sitemap_ip.xml", function(err,sites){
- 	if(!err)console.log(sites);else console.log(err);
-});
+// sitemap.getSites("http://www.walmart.com/sitemap_ip.xml", function(err,sites){
+//  	if(!err)console.log(sites);else console.log(err);
+// });
 
 // sitemap.getSites("http://www.rakuten.com/sitemapxml/sitemapindex.xml", function(err,sites){
 //  	if(!err)console.log(sites);else console.log(err);
