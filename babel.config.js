db462a7b9dd915dda39555653023fd1abfa0314b
Sean Thomas Burke <965298+seantomburke@users.noreply.github.com>
2020-01-03 12:07:35 -0800

    Adding es5 Module support (#41)
    * Adding compatibility for es5 and es6 modules
    
    * removing unused dependency

--- babel.config.js
+++ babel.config.js
@@ -2,7 +2,7 @@ module.exports = (api) => {
   api.cache(true);
 
   const presets = ['@babel/preset-env'];
-  const plugins = [];
+  const plugins = ['add-module-exports'];
 
   return {
     presets,
