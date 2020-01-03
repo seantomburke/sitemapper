d1a2df725b9a35a84888777879cec52cf945178e
Sean Thomas Burke <seantomburke@users.noreply.github.com>
2020-01-02 17:36:38 -0800

    Upgrading eslint dependency

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  extends: 'eslint:recommended',
+  parserOptions: {
+    ecmaVersion: 6,
+    sourceType: 'module',
+    ecmaFeatures: {},
+  },
+  env: {
+    node: true,
+    mocha: true,
+  },
+};
--- src/assets/sitemapper.js
+++ src/assets/sitemapper.js
@@ -1,5 +1,3 @@
-/* global require,module */
-
 /**
  * Sitemap Parser
  *
