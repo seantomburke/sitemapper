9372b540b2ad41c1bed8a8e54cb84e7fad9f6a29
Panagiotis Tzamtzis <panagiotis@baresquare.com>
2021-02-10 21:21:21 +0200

    New features & updated documentation
    # New features added
    
    * Ability to report on sitemap crawl errors in returned results. Added a new "errors" property in the `SitesData` object
    
    * Added an option to set a concurrency limit to rate limit sitemap crawling. Useful when crawling sitemaps with multiple children to avoid getting blocked by firewalls. #77
    
    * Added an option to have retry requests upon failure and to set the number of maximum retries per crawl.
    
    # Documentation changes
    
    * Updated documentation to include all the new features described above.
    
    Co-Authored-By: Panagiotis Tzamtzis <panagiotis@baresquare.com>
    Co-Authored-By: PanagiotisTzamtzis <panagiotis@tzamtzis.gr>

--- src/examples/index.js
+++ src/examples/index.js
@@ -4,10 +4,13 @@ import Sitemapper from '../assets/sitemapper';
 const exampleURL = 'https://www.walmart.com/sitemap_topic.xml';
 
 // Instantiate an instance
-const sitemapper = new Sitemapper({
+let sitemapper = new Sitemapper({
   url: exampleURL, // url to crawl
-  debug: true, // don't show debug logs
-  timeout: 1, // 10 seconds
+  debug: false, // don't show debug logs
+  timeout: 10000, // 10 seconds
+  concurrency: 10, // Number of maximum concurrent sitemap crawl threads
+  retries: 0, // Number of retry attempts in case of error response (e.g. 404 or timeout)
+  returnErrors: true
 });
 
 /**
