975f1849c133e041e1410fd4dc2603c2a3675d56
Sean Thomas Burke <seantomburke@users.noreply.github.com>
2024-11-07 22:34:57 -0800

    Fixing ESlint

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -26,7 +26,6 @@ export default [
       'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
       'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
       'prettier/prettier': 'error',
-      ...prettier.rules,
       ...eslintPluginMocha.configs.recommended.rules,
     },
   },
