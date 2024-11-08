4fe44a294ee97b8d21446a361812f4c2ee6d9de1
Sean Thomas Burke <965298+seantomburke@users.noreply.github.com>
2024-11-07 23:27:54 -0800

    Update Packages (#156)
    * Updating mocha package
    
    * Updating spacing, updating eslint, removing documentation, fixing lint issues
    
    * Updating
    
    * Use mocha globals
    
    * Fixing ESlint
    
    * Fixing ESlint
    
    * Updating Docs
    
    * Updating CodeQL
    
    * Updating style
    
    * Updating checkout action
    
    ---------
    
    Co-authored-by: Sean Thomas Burke <seantomburke@users.noreply.github.com>

--- README.md
+++ README.md
@@ -1,9 +1,9 @@
 ## Sitemap-parser
+
 [![Code Scanning](https://github.com/seantomburke/sitemapper/actions/workflows/codeql-analysis.yml/badge.svg)](https://github.com/seantomburke/sitemapper/actions/workflows/codeql-analysis.yml)
 [![NPM Publish](https://github.com/seantomburke/sitemapper/actions/workflows/npm-publish.yml/badge.svg)](https://github.com/seantomburke/sitemapper/actions/workflows/npm-publish.yml)
 [![Version Bump](https://github.com/seantomburke/sitemapper/actions/workflows/version-bump.yml/badge.svg?branch=master&event=push)](https://github.com/seantomburke/sitemapper/actions/workflows/version-bump.yml)
 [![Test](https://github.com/seantomburke/sitemapper/actions/workflows/test.yml/badge.svg?branch=master&event=push)](https://github.com/seantomburke/sitemapper/actions/workflows/test.yml)
-[![Build Status](https://travis-ci.org/seantomburke/sitemapper.svg?branch=master)](https://travis-ci.org/seantomburke/sitemapper)
 [![Codecov](https://img.shields.io/codecov/c/github/seantomburke/sitemapper?token=XhiEgaHFWL)](https://codecov.io/gh/seantomburke/sitemapper)
 [![CodeFactor](https://www.codefactor.io/repository/github/seantomburke/sitemapper/badge)](https://www.codefactor.io/repository/github/seantomburke/sitemapper)
 [![GitHub license](https://img.shields.io/github/license/seantomburke/sitemapper)](https://github.com/seantomburke/sitemapper/blob/master/LICENSE)
@@ -17,25 +17,29 @@
 [![scrutinizer](https://img.shields.io/scrutinizer/quality/g/seantomburke/sitemapper.svg?style=flat-square)](https://scrutinizer-ci.com/g/seantomburke/sitemapper/)
 
 Parse through a sitemaps xml to get all the urls for your crawler.
+
 ## Version 2
 
 ### Installation
+
 ```bash
 npm install sitemapper --save
 ```
 
 ### Simple Example
+
 ```javascript
 const Sitemapper = require('sitemapper');
 
 const sitemap = new Sitemapper();
 
-sitemap.fetch('https://wp.seantburke.com/sitemap.xml').then(function(sites) {
+sitemap.fetch('https://wp.seantburke.com/sitemap.xml').then(function (sites) {
   console.log(sites);
 });
-
 ```
-### Examples in ES6
+
+### Examples
+
 ```javascript
 import Sitemapper from 'sitemapper';
 
@@ -58,106 +62,71 @@ import Sitemapper from 'sitemapper';
 const sitemapper = new Sitemapper();
 sitemapper.timeout = 5000;
 
-sitemapper.fetch('https://wp.seantburke.com/sitemap.xml')
+sitemapper
+  .fetch('https://wp.seantburke.com/sitemap.xml')
   .then(({ url, sites }) => console.log(`url:${url}`, 'sites:', sites))
-  .catch(error => console.log(error));
+  .catch((error) => console.log(error));
 ```
 
 # Options
 
 You can add options on the initial Sitemapper object when instantiating it.
 
-+ `requestHeaders`: (Object) - Additional Request Headers (e.g. `User-Agent`)
-+ `timeout`: (Number) - Maximum timeout in ms for a single URL. Default: 15000 (15 seconds)
-+ `url`: (String) - Sitemap URL to crawl
-+ `debug`: (Boolean) - Enables/Disables debug console logging. Default: False
-+ `concurrency`: (Number) - Sets the maximum number of concurrent sitemap crawling threads. Default: 10
-+ `retries`: (Number) - Sets the maximum number of retries to attempt in case of an error response (e.g. 404 or Timeout). Default: 0
-+ `rejectUnauthorized`: (Boolean) - If true, it will throw on invalid certificates, such as expired or self-signed ones. Default: True
-+ `lastmod`: (Number) - Timestamp of the minimum lastmod value allowed for returned urls
-+ `field` : (Object) - An object of fields to be returned from the sitemap. For Example: `{ loc: true, lastmod: true, changefreq: true, priority: true }`. Leaving a field out has the same effect as `field: false`. If not specified sitemapper defaults to returning the 'classic' array of urls.
-+ `proxyAgent`: (HttpProxyAgent|HttpsProxyAgent) - instance of npm "hpagent" HttpProxyAgent or HttpsProxyAgent to be passed to npm "got"
+- `requestHeaders`: (Object) - Additional Request Headers (e.g. `User-Agent`)
+- `timeout`: (Number) - Maximum timeout in ms for a single URL. Default: 15000 (15 seconds)
+- `url`: (String) - Sitemap URL to crawl
+- `debug`: (Boolean) - Enables/Disables debug console logging. Default: False
+- `concurrency`: (Number) - Sets the maximum number of concurrent sitemap crawling threads. Default: 10
+- `retries`: (Number) - Sets the maximum number of retries to attempt in case of an error response (e.g. 404 or Timeout). Default: 0
+- `rejectUnauthorized`: (Boolean) - If true, it will throw on invalid certificates, such as expired or self-signed ones. Default: True
+- `lastmod`: (Number) - Timestamp of the minimum lastmod value allowed for returned urls
+- `proxyAgent`: (HttpProxyAgent|HttpsProxyAgent) - instance of npm "hpagent" HttpProxyAgent or HttpsProxyAgent to be passed to npm "got"
+- `exclusions`: (Array<RegExp>) - Array of regex patterns to exclude URLs from being processed
+- `field`: (Object) - An object of fields to be returned from the sitemap. Leaving a field out has the same effect as `<field>: false`. If not specified sitemapper defaults to returning the 'classic' array of urls. Available fields:
+  - `loc`: (Boolean) - The URL location of the page
+  - `lastmod`: (Boolean) - The date of last modification of the page
+  - `changefreq`: (Boolean) - How frequently the page is likely to change
+  - `priority`: (Boolean) - The priority of this URL relative to other URLs on your site
+  - `image:loc`: (Boolean) - The URL location of the image (for image sitemaps)
+  - `image:title`: (Boolean) - The title of the image (for image sitemaps)
+  - `image:caption`: (Boolean) - The caption of the image (for image sitemaps)
+  - `video:title`: (Boolean) - The title of the video (for video sitemaps)
+  - `video:description`: (Boolean) - The description of the video (for video sitemaps)
+  - `video:thumbnail_loc`: (Boolean) - The thumbnail URL of the video (for video sitemaps)
 
-```javascript
-
-const sitemapper = new Sitemapper({
-  url: 'https://art-works.community/sitemap.xml',
-  rejectUnauthorized: true,
-  timeout: 15000,
-  requestHeaders: {
-    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:81.0) Gecko/20100101 Firefox/81.0'
-  }
-});
+For Example:
 
 ```
+field: {
+  loc: true,
+  lastmod: true,
+  changefreq: true,
+  priority: true,
+}
+```
+
+Leaving a field out has the same effect as `<field>: false`. If not specified sitemapper defaults to returning the 'classic' array of urls.
 
 An example using all available options:
 
 ```javascript
-
 const sitemapper = new Sitemapper({
   url: 'https://art-works.community/sitemap.xml',
   timeout: 15000,
   requestHeaders: {
-    'User-Agent': 'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:81.0) Gecko/20100101 Firefox/81.0'
+    'User-Agent':
+      'Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:81.0) Gecko/20100101 Firefox/81.0',
   },
   debug: true,
   concurrency: 2,
   retries: 1,
-});
-
-```
-
-### Examples in ES5
-```javascript
-var Sitemapper = require('sitemapper');
-
-var Google = new Sitemapper({
-  url: 'https://www.google.com/work/sitemap.xml',
-  timeout: 15000 //15 seconds
-});
-
-Google.fetch()
-  .then(function (data) {
-    console.log(data);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-
-
-// or
-
-
-var sitemapper = new Sitemapper();
-
-sitemapper.timeout = 5000;
-sitemapper.fetch('https://wp.seantburke.com/sitemap.xml')
-  .then(function (data) {
-    console.log(data);
-  })
-  .catch(function (error) {
-    console.log(error);
-  });
-
-```
-
-## Version 1
-
-```bash
-npm install sitemapper@1.1.1 --save
-```
-
-### Simple Example
-
-```javascript
-var Sitemapper = require('sitemapper');
-
-var sitemapper = new Sitemapper();
-
-sitemapper.getSites('https://wp.seantburke.com/sitemap.xml', function(err, sites) {
-    if (!err) {
-     console.log(sites);
-    }
+  rejectUnauthorized: false,
+  field: {
+    loc: true,
+    lastmod: true,
+    changefreq: true,
+    priority: true,
+  },
+  proxyAgent: new HttpProxyAgent('http://localhost:8080'),
 });
 ```
--- src/tests/test.ts.ts
+++ src/tests/test.ts.ts
@@ -9,13 +9,11 @@ import { SitemapperResponse } from '../../sitemapper';
 let sitemapper: Sitemapper;
 
 describe('Sitemapper', function () {
-
   beforeEach(() => {
     sitemapper = new Sitemapper();
   });
 
   describe('Sitemapper Class', function () {
-
     it('should have initializeTimeout method', () => {
       sitemapper.initializeTimeout.should.be.Function;
     });
@@ -58,30 +56,32 @@ describe('Sitemapper', function () {
 
     it('should construct with specific fields', () => {
       sitemapper = new Sitemapper({
-        fields: { "loc": true,
-          "lastmod": true,
-          "priority": true,
-          "changefreq": true
-        }
+        fields: { loc: true, lastmod: true, priority: true, changefreq: true },
       });
-      sitemapper.fields.should.be.Object && sitemapper.fields.should.have.keys('loc', 'lastmod', 'priority', 'changefreq');
+      sitemapper.fields.should.be.Object &&
+        sitemapper.fields.should.have.keys(
+          'loc',
+          'lastmod',
+          'priority',
+          'changefreq'
+        );
     });
-
   });
 
   describe('fetch Method resolves sites to array', function () {
     it('https://wp.seantburke.com/sitemap.xml sitemaps should be an array', function (done) {
       this.timeout(30000);
       const url = 'https://wp.seantburke.com/sitemap.xml';
-      sitemapper.fetch(url)
-        .then(data => {
+      sitemapper
+        .fetch(url)
+        .then((data) => {
           data.sites.should.be.Array;
           data.url.should.equal(url);
           data.sites.length.should.be.above(2);
           isUrl(data.sites[0]).should.be.true;
           done();
         })
-        .catch(error => {
+        .catch((error) => {
           console.error('Test failed');
           done(error);
         });
@@ -90,13 +90,17 @@ describe('Sitemapper', function () {
     it('gibberish.gibberish should fail silently with an empty array', function (done) {
       this.timeout(30000);
       const url = 'http://gibberish.gibberish';
-      sitemapper.fetch(url)
-        .then(data => {
+      sitemapper.debug = true;
+      sitemapper
+        .fetch(url)
+        .then((data) => {
           data.sites.should.be.Array;
           data.errors.should.be.Array;
+          data.errors.length.should.be.greaterThan(0);
+          console.log(data);
           done();
         })
-        .catch(error => {
+        .catch((error) => {
           console.error('Test failed');
           done(error);
         });
@@ -105,15 +109,16 @@ describe('Sitemapper', function () {
     it('https://webflow.com/sitemap.xml sitemaps should be an array', function (done) {
       this.timeout(30000);
       const url = 'https://webflow.com/sitemap.xml';
-      sitemapper.fetch(url)
-        .then(data => {
+      sitemapper
+        .fetch(url)
+        .then((data) => {
           data.sites.should.be.Array;
           data.url.should.equal(url);
           data.sites.length.should.be.above(2);
           isUrl(data.sites[0]).should.be.true;
           done();
         })
-        .catch(error => {
+        .catch((error) => {
           console.error('Test failed');
           done(error);
         });
@@ -123,42 +128,40 @@ describe('Sitemapper', function () {
       this.timeout(30000);
       const url = 'https://wp.seantburke.com/sitemap.xml';
       sitemapper = new Sitemapper({
-        fields: { "loc": true,
-          "lastmod": true,
-          "priority": true,
-          "changefreq": true
-        }
+        fields: { loc: true, lastmod: true, priority: true, changefreq: true },
       });
-      sitemapper.fetch(url)
-          .then(data => {
-            data.sites.should.be.Array;
-            data.url.should.equal(url);
-            data.sites.length.should.be.above(2);
-            data.sites[0].loc.should.be.String;
-            data.sites[0].lastmod.should.be.String;
-            data.sites[0].priority.should.be.String;
-            data.sites[0].changefreq.should.be.String;
-            done();
-          })
-          .catch(error => {
-            console.error('Test failed');
-            done(error);
-          });
+      sitemapper
+        .fetch(url)
+        .then((data) => {
+          data.sites.should.be.Array;
+          data.url.should.equal(url);
+          data.sites.length.should.be.above(2);
+          data.sites[0].loc.should.be.String;
+          data.sites[0].lastmod.should.be.String;
+          data.sites[0].priority.should.be.String;
+          data.sites[0].changefreq.should.be.String;
+          done();
+        })
+        .catch((error) => {
+          console.error('Test failed');
+          done(error);
+        });
     });
 
     it('https://www.golinks.io/sitemap.xml sitemaps should be an array', function (done) {
       this.timeout(30000);
       const url = 'https://www.golinks.io/sitemap.xml';
       sitemapper.timeout = 5000;
-      sitemapper.fetch(url)
-        .then(data => {
+      sitemapper
+        .fetch(url)
+        .then((data) => {
           data.sites.should.be.Array;
           data.url.should.equal(url);
           data.sites.length.should.be.above(2);
           isUrl(data.sites[0]).should.be.true;
           done();
         })
-        .catch(error => {
+        .catch((error) => {
           console.error('Test failed');
           done(error);
         });
@@ -168,13 +171,49 @@ describe('Sitemapper', function () {
       this.timeout(30000);
       const url = 'https://www.golinks.io/sitemap.xml';
       sitemapper.timeout = 1;
-
-      sitemapper.fetch(url)
-        .then(data => {
+      sitemapper
+        .fetch(url)
+        .then((data) => {
           data.sites.should.be.Array;
+          data.errors.should.be.Array;
+          console.log(data);
           done();
         })
-        .catch(error => {
+        .catch((error) => {
+          console.error('Test failed');
+          done(error);
+        });
+    });
+
+    it('https://www.golinks.com/blog/sitemap.xml sitemaps should return an empty array when timing out', function (done) {
+      this.timeout(30000);
+      const url = 'https://www.golinks.com/blog/sitemap.xml';
+      sitemapper.timeout = 10000;
+      sitemapper
+        .fetch(url)
+        .then((data) => {
+          data.sites.should.be.Array;
+          data.errors.should.be.Array;
+          done();
+        })
+        .catch((error) => {
+          console.error('Test failed');
+          done(error);
+        });
+    });
+
+    it('https://www.banggood.com/sitemap/category.xml.gz gzip should be a non-empty array', function (done) {
+      this.timeout(30000);
+      const url = 'https://www.banggood.com/sitemap/category.xml.gz';
+      sitemapper.timeout = 10000;
+      sitemapper
+        .fetch(url)
+        .then((data) => {
+          data.sites.should.be.Array;
+          data.sites.length.should.be.greaterThan(0);
+          done();
+        })
+        .catch((error) => {
           console.error('Test failed');
           done(error);
         });
@@ -186,7 +225,7 @@ describe('Sitemapper', function () {
       sitemapper = new Sitemapper({
         requestHeaders: {
           'Accept-Encoding': 'gzip,deflate,sdch',
-        }
+        },
       });
     });
 
@@ -194,14 +233,39 @@ describe('Sitemapper', function () {
       this.timeout(30000);
       const url = 'https://www.banggood.com/sitemap/category.xml.gz';
       sitemapper.timeout = 10000;
-      sitemapper.fetch(url)
-        .then(data => {
+      sitemapper
+        .fetch(url)
+        .then((data) => {
           data.sites.should.be.Array;
           data.errors.should.be.Array;
           data.sites.length.should.be.greaterThan(0);
           done();
         })
-        .catch(error => {
+        .catch((error) => {
+          console.error('Test failed');
+          done(error);
+        });
+    });
+
+    it('https://foo.com/sitemap.xml should not allow insecure request', function (done) {
+      this.timeout(30000);
+      const url = 'https://foo.com/sitemap.xml';
+      sitemapper.timeout = 10000;
+      sitemapper.rejectUnauthorized = false;
+      sitemapper
+        .fetch(url)
+        .then((data) => {
+          data.sites.should.be.Array;
+          data.errors.should.be.Array;
+          data.errors.should.containEql({
+            type: 'HTTPError',
+            message: 'HTTP Error occurred: Response code 404 (Not Found)',
+            url: 'https://foo.com/sitemap.xml',
+            retries: 0,
+          });
+          done();
+        })
+        .catch((error) => {
           console.error('Test failed');
           done(error);
         });
@@ -217,6 +281,85 @@ describe('Sitemapper', function () {
         isUrl(sites[0]).should.be.true;
         done();
       });
+    });
+  });
+
+  describe('exclusions option', function () {
+    it('should prevent false positive', function (done) {
+      this.timeout(30000);
+      const url = 'https://wp.seantburke.com/sitemap.xml';
+      sitemapper.exclusions = [/video/, /image/];
+      sitemapper
+        .fetch(url)
+        .then((data) => {
+          data.sites.should.be.Array;
+          data.sites.includes('https://wp.seantburke.com/?page_id=2').should.be
+            .true;
+          done();
+        })
+        .catch((error) => {
+          console.error('Test failed');
+          done(error);
+        });
+    });
+
+    it('should filter out page_id urls', function (done) {
+      this.timeout(30000);
+      const url = 'https://wp.seantburke.com/sitemap.xml';
+      sitemapper.exclusions = [/page_id/];
+      sitemapper
+        .fetch(url)
+        .then((data) => {
+          data.sites.should.be.Array;
+          data.sites.includes('https://wp.seantburke.com/?page_id=2').should.be
+            .false;
+          done();
+        })
+        .catch((error) => {
+          console.error('Test failed');
+          done(error);
+        });
+    });
+  });
+
+  describe('isExcluded method', function () {
+    it('should return false when no exclusions are set', function () {
+      const result = sitemapper.isExcluded('https://foo.com/page1');
+      result.should.be.false();
+    });
+
+    it('should return false when url does not match any exclusion patterns', function () {
+      sitemapper.exclusions = [/\.pdf$/, /private/];
+      const result = sitemapper.isExcluded('https://foo.com/page1');
+      result.should.be.false();
+    });
+
+    it('should return false when url matches an exclusion pattern', function () {
+      sitemapper.exclusions = [/\.pdf$/, /private/];
+      const result = sitemapper.isExcluded('https://foo.com/document.pdf');
+      result.should.be.true();
+    });
+
+    it('should return true when url matches any of multiple exclusion patterns', function () {
+      sitemapper.exclusions = [/\.pdf$/, /private/, /temp/];
+      const result = sitemapper.isExcluded('https://foo.com/private/temp.html');
+      result.should.be.true();
+    });
+
+    it('should handle complex regex patterns correctly', function () {
+      sitemapper.exclusions = [/^https:\/\/foo\.com\/([a-z]{2})\/private/];
+      const result1 = sitemapper.isExcluded('https://foo.com/en/private/page');
+      const result2 = sitemapper.isExcluded('https://foo.com/en/public/page');
+      result1.should.be.true();
+      result2.should.be.false();
+    });
+
+    it('should handle case sensitivity correctly', function () {
+      sitemapper.exclusions = [/private/i];
+      const result1 = sitemapper.isExcluded('https://foo.com/PRIVATE/page');
+      const result2 = sitemapper.isExcluded('https://foo.com/Private/page');
+      result1.should.be.true();
+      result2.should.be.true();
     });
   });
 });
