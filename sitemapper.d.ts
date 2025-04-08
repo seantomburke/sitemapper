b6cdded1a92b5c56237fb68251edaf0aff6996b1
Sean Thomas Burke <seantomburke@users.noreply.github.com>
2025-04-08 15:31:05 -0700

    Fixing typescript tests

--- sitemapper.d.ts
+++ sitemapper.d.ts
@@ -1,6 +1,14 @@
+export interface SitemapperSiteData {
+  loc: string;
+  lastmod?: string;
+  priority?: string;
+  changefreq?: string;
+  [key: string]: any;
+}
+
 export interface SitemapperResponse {
   url: string;
-  sites: string[];
+  sites: string[] | SitemapperSiteData[];
   errors: SitemapperErrorData[];
 }
 
@@ -20,6 +28,7 @@ export interface SitemapperOptions {
   timeout?: number;
   url?: string;
   fields?: { [name: string]: boolean };
+  proxyAgent?: any;
   exclusions?: RegExp[];
 }
 
@@ -28,15 +37,44 @@ declare class Sitemapper {
   url: string;
   debug: boolean;
   lastmod: number;
+  fields?: { [name: string]: boolean };
+  requestHeaders?: { [name: string]: string };
+  concurrency?: number;
+  retries?: number;
+  rejectUnauthorized?: boolean;
+  exclusions?: RegExp[];
+  proxyAgent?: any;
+  timeoutTable: { [url: string]: NodeJS.Timeout };
 
   constructor(options?: SitemapperOptions);
+
+  private initializeTimeout(url: string, requester: any): void;
+  private crawl(url: string, retryIndex?: number): Promise<any>;
+  private parse(url: string): Promise<any>;
+  isExcluded(url: string): boolean;
 
   /**
    * Gets the sites from a sitemap.xml with a given URL
    *
    * @param url URL to the sitemap.xml file
    */
-  fetch(url?: string): Promise<SitemapperResponse>;
+  fetch(
+    this: Sitemapper & { fields: object },
+    url?: string
+  ): Promise<
+    Omit<SitemapperResponse, 'sites'> & { sites: SitemapperSiteData[] }
+  >;
+  fetch(
+    url?: string
+  ): Promise<Omit<SitemapperResponse, 'sites'> & { sites: string[] }>;
+
+  /**
+   * @deprecated Use fetch() instead.
+   */
+  getSites(
+    url: string | undefined,
+    callback: (err: Error | null, sites: string[]) => void
+  ): Promise<void>;
 }
 
 export default Sitemapper;
