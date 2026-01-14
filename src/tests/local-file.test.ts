0d909da7490bfcae06ffd4f12341680c21ac4d2a
Lee Hazlett <Lee.Hazlett@tylertech.com>
2026-01-14 13:41:11 -0500

    adding path back for test

--- src/tests/local-file.test.ts
+++ src/tests/local-file.test.ts
@@ -3,6 +3,7 @@ import 'assert';
 import 'should';
 import { fileURLToPath } from 'url';
 
+const path = require('path');
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
